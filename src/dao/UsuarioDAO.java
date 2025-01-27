6a7b2b1eea68ab2589ee42b8fca7409174526046
JohnJohn_081 <johnmaclebio@gmail.com>
2025-01-27 15:10:43 -0300

    Otimização do App.java

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -290,5 +290,51 @@ public class UsuarioDAO {
         }
 
     }
+
+    public void printarMensagemCadastroAdmin (Usuario usuario){
+        System.out.println("---------------------------------");
+        System.out.println("        BEM-VINDO                ");
+        System.out.println("-                                ");
+        System.out.println("-  1- Cadastrar Aluno            ");
+        System.out.println("-  2- Editar aluno               ");
+        System.out.println("-  3- Voltar                     ");
+        System.out.println("-                                ");
+        System.out.println("---------------------------------");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    public void printarMensagemPainelAluno(Aluno aluno){
+        System.out.println("---------------------------------");
+        System.out.println("        BEM-VINDO                ");
+        System.out.println("-                                ");
+        System.out.println("-  1- Verificar perfil           ");
+        System.out.println("-  2- Cronograma                 ");
+        System.out.println("-  3- Exit                       ");
+        System.out.println("-                                ");
+        System.out.println("---------------------------------");
+        System.out.print("Digite uma opção: ");
+    }
+
+    public void printarConogramaAluno(Aluno aluno){
+        System.out.println("+----------------+-----------+-----------+-----------+-----------+-----------+");
+        System.out.println("|     Horário    |  Segunda  |   Terça   |  Quarta   |   Quinta  |   Sexta   |");
+        System.out.println("+----------------+-----------+-----------+-----------+-----------+-----------+");
+        System.out.println("|  7:30 - 8:20   | Matemática|   Inglês  |   Artes   |  História | Geografia |");
+        System.out.println("|  8:20 - 9:10   | Matemática|   Química | Filosofia | Ed. Física| Português |");
+        System.out.println("|  9:10 - 9:30   |    ------- Intervalo -------                              |");
+        System.out.println("|  9:30 - 10:20  | Geografia | Matemática| Português |   Inglês  |  Química  |");
+        System.out.println("| 10:20 - 11:10  |   Artes   |  História | Filosofia | Ed. Física| Matemática|");
+        System.out.println("| 11:10 - 12:00  | Português |   Inglês  | Matemática|  História |  Química  |");
+        System.out.println("+----------------+-----------+-----------+-----------+-----------+-----------+");
+        System.out.println("| 12:00 - 13:00  |                 Almoço                                    |");
+        System.out.println("+----------------+-----------+-----------+-----------+-----------+-----------+");
+        System.out.println("| 13:00 - 13:50  |   Química | Português | Matemática| Filosofia | Geografia |");
+        System.out.println("| 13:50 - 14:40  |  História | Ed. Física|   Inglês  | Português | Matemática|");
+        System.out.println("| 14:40 - 15:00  |    ------- Intervalo -------                              |");
+        System.out.println("| 15:00 - 15:50  | Matemática| Português |   Química |  História | Filosofia |");
+        System.out.println("| 15:50 - 16:40  |   Inglês  |   Artes   | Geografia | Matemática| Ed. Física|");
+        System.out.println("+----------------+-----------+-----------+-----------+-----------+-----------+");
+
+    }
 }
 
